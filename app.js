32bde538bc48ef3a6c2d0250bab1cbc2dbe3228a
wewilliams9999 <williamswallise@gmail.com>
2022-06-26 17:58:04 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Wallis";
+let lastName = "Williams";
+var age = 36;
 
 // Exercise 2
+let fullName = firstName + " " + lastName; //concatenation
+
+let templateFullName = `${firstName} ${lastName}`; //must use backticks for string interpolation in JavaScript - template literal
+
+console.log("fullName: ", fullName);
+console.log("templateFullName: ", templateFullName);
 
 // Exercise 3
+let city = "Nashville";
+let pastime = "watching TV";
+
+let myStory = `Hello, my name is ${fullName}.
+I live in ${city}. I really love ${pastime}.`
+
+console.log(myStory);
